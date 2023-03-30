98be4996086b9f6adf14875c1fd681087cd8122e
John Velo <80282650+johnnvelo7@users.noreply.github.com>
2023-03-30 15:06:46 +0200

    Update arbeidskrav.py

--- arbeidsKrav/arbeidskrav.py
+++ arbeidsKrav/arbeidskrav.py
@@ -1,4 +1,5 @@
-quiz_questions = [
+# Tuples and Dictionary
+quiz_questions = (
     "What is the capital of Norway?",
     "What is the currency of Norway?",
     "What is the largest city in Norway?",
@@ -9,20 +10,20 @@ quiz_questions = [
     "How long is the border between Norway and Russia?",
     "Where in Norway is Stavanger?",
     "From which Norwegian city did the world’s famous composer Edvard Grieg come?",
-]
-quiz_answers = [
-    ["Oslo", "Bergen", "Trondheim", "Stavanger"],
-    ["Euro", "Pound", "Krone", "Deutsche Mark"],
-    ["Oslo", "Stavanger", "Bergen", "Trondheim"],
-    ["27th May", "17th May", "17th April", "27th April"],
-    ["Red", "White", "Blue", "Yellow"],
-    ["1", "2", "3", "4"],
-    ["UiS", "UiO", "NMBU", "NTNU"],
-    ["96km", "196km", "296km", "396km"],
-    ["North", "South", "South-West", "South-East"],
-    ["Oslo", "Bergen", "Stavanger", "Tromsø"]
-]
-correct_answers = [
+)
+quiz_answers = (
+    ("Oslo", "Bergen", "Trondheim", "Stavanger"),
+    ("Euro", "Pound", "Krone", "Deutsche Mark"),
+    ("Oslo", "Stavanger", "Bergen", "Trondheim"),
+    ("27th May", "17th May", "17th April", "27th April"),
+    ("Red", "White", "Blue", "Yellow"),
+    ("1", "2", "3", "4"),
+    ("UiS", "UiO", "NMBU", "NTNU"),
+    ("96km", "196km", "296km", "396km"),
+    ("North", "South", "South-West", "South-East"),
+    ("Oslo", "Bergen", "Stavanger", "Tromsø")
+)
+correct_answers = (
     "Oslo",
     "Krone",
     "Oslo",
@@ -33,12 +34,12 @@ correct_answers = [
     "196km",
     "South-West",
     "Bergen"
-]
-
+)
 login_info_dict = {'PGR107': 'Python'}
 
 # LOGIN FUNCTION
 def Login_info():
+    # User input validation
     while True:
         username = input("Please enter your username: ")
         password = input("Please enter your password: ")
@@ -48,17 +49,17 @@ def Login_info():
         else:
             print("Invalid username and/or password. Please try again.")
 
+
 # QUIZ FUNCTION
 def Quiz():
     num_correct_answers = 0
     num_wrong_answers = 0
     user_answers = []
-
     for question_number, question in enumerate(quiz_questions):
         print(f"Question {question_number + 1}: {question}")
         for i, answer in enumerate(quiz_answers[question_number]):
             print(f"{i + 1}. {answer}")
-
+        # User input validation
         while True:
             user_answer = input("Enter your answer (1-4): ")
             if user_answer.isdigit() and 1 <= int(user_answer) <= 4:
@@ -66,9 +67,9 @@ def Quiz():
                 break
             else:
                 print("Please enter a valid answer (1-4).")
-
+        # Append user answers
         user_answers.append(user_answer)
-
+        # Check if answer is correct
         if quiz_answers[question_number][user_answer - 1] == correct_answers[question_number]:
             print("Correct!\n")
             num_correct_answers += 1
@@ -76,9 +77,8 @@ def Quiz():
             print("Wrong!\n")
             num_wrong_answers += 1
 
-    print(
-        f"Quiz completed! You answered {num_correct_answers} questions correctly and {num_wrong_answers} questions incorrectly.\n")
-
+    print(f"Quiz completed! You answered {num_correct_answers} questions correctly and {num_wrong_answers} questions incorrectly.\n")
+    # Print wrong answers with correct answers
     if num_wrong_answers > 0:
         print("Here are the correct answers for the questions you got wrong:")
         for question_number, question in enumerate(quiz_questions):
